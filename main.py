4a7aa27291d4b89d0ca71ebf6376056858c6af30
kermit-frogs19 <dadandboom@gmail.com>
2025-05-07 17:24:08 +0300

    chore: Dockerfile added, server configured

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
-# This is a sample Python script.
+from ezRPC.receiver.receiver import Receiver
+from ezRPC.producer.producer import Producer
 import asyncio
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from ezh3 import Client
-from ezRPC import Producer
+
+app = Receiver(
+    enable_tls=True,
+    cert_type="CUSTOM",
+    custom_cert_file_loc="/app/cert.pem",
+    custom_cert_key_file_loc="/app/key.pem",
+    port=8080,
+    enable_ipv6=False
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.function(description="Get a sum of 2 integers")
+async def get_sum(a: int, b: int) -> int:
+    print(a)
+    print(b)
+    result = a + b
+    return result
+
+
+@app.function(description="Get a subtraction of 2 integers")
+async def get_subtraction(a: int, b: int) -> int:
+    print(a)
+    print(b)
+    result = a - b
+    return result
 
 
 async def sender():
-    client = Producer("https://[fe80::bb3b:fde9:23a5:1a5f%8]:8000", use_tls=False, timeout=None)   # "https://vadim-seliukov-quic-server.com"
+    client = Producer("https://vadim-seliukov-quic-server.com:8080", use_tls=True)
+    result = await client.call("get_sum", 1234, 1234)
+    print(result)
+    await client.close()
 
-    try:
-        sum = await client.call("get_sum", 125, 97)
-        print(sum)
-
-        subtraction = await client.call("get_subtraction", 125, 97)
-        print(subtraction)
-
-        subtraction = await client.rpc.get_subtraction(125, 97)
-        print(subtraction)
-
-
-        difference = await client.call("get_difference", 125, 97)
-        print(difference)
-    except BaseException as e:
-        print(e)
-    finally:
-        await client.close()
     print("done")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    asyncio.run(sender())
+if __name__ == "__main__":
+    asyncio.run(app.run())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
