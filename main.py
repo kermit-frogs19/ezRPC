de95ce3494692f5c8b530dd32df687cb0e8cb294
kermit-frogs19 <dadandboom@gmail.com>
2025-05-07 17:43:48 +0300

    chore: IPv6 enabled for server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ezRPC.receiver.receiver import Receiver
 from ezRPC.producer.producer import Producer
+from ezh3 import Client
 import asyncio
 
 
@@ -9,7 +10,7 @@ app = Receiver(
     custom_cert_file_loc="/app/cert.pem",
     custom_cert_key_file_loc="/app/key.pem",
     port=8080,
-    enable_ipv6=False
+    enable_ipv6=True
 )
 
 
@@ -32,13 +33,46 @@ async def get_subtraction(a: int, b: int) -> int:
 async def sender():
     client = Producer("https://vadim-seliukov-quic-server.com:8080", use_tls=True)
     result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+    result = await client.call("get_sum", 1234, 1234)
+
     print(result)
     await client.close()
 
     print("done")
 
+async def sender_server():
+    client = Client("https://vadim-seliukov-quic-server.com:8000", use_tls=True)
+    data = {"voice": "Hellooo"}
+
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+    response = await client.post("/echo", json=data, timeout=None)
+
+    await client.close()
+    print(response)
+
+
 
 if __name__ == "__main__":
+    # asyncio.run(sender())
+    # asyncio.run(sender_server())
+
     asyncio.run(app.run())
 
 
