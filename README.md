b9683c27e804b6b3d6329aa83f68d3f046ff2e80
kermit-frogs19 <dadandboom@gmail.com>
2025-05-17 01:27:20 +0300

    style: README added, and examples cleaned

---
+++ README.md
@@ -0,0 +1,9 @@
+## Convenient HTTP/3 & MessagePack based RPC.
+
+Download with:
+
+`pip install git+https://github.com/kermit-frogs19/ezRPC.git`
+
+You'll find examples in:
+- `main.py`
+- `example.py`
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ezRPC.producer.producer import Producer
 from ezh3 import Client
 import asyncio
 import time
+import logging
 
 try:
     import uvloop
@@ -15,15 +16,20 @@ except ImportError:
 
 
 app = Receiver(
-    enable_tls=True,
+    enable_tls=False,
     custom_cert_file_loc="/app/cert.pem",
     custom_cert_key_file_loc="/app/key.pem",
     port=8080,
     enable_ipv6=True
 )
 
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+    level=logging.DEBUG,
+)
 
-@app.function(description="Get a sum of 2 integers")
+
+@app.function()
 async def get_sum(a: int, b: int) -> int:
     print(a)
     print(b)
@@ -31,7 +37,7 @@ async def get_sum(a: int, b: int) -> int:
     return result
 
 
-@app.function(description="Get a subtraction of 2 integers")
+@app.function()
 async def get_subtraction(a: int, b: int) -> int:
     print(a)
     print(b)
@@ -44,24 +50,41 @@ async def dummy():
     return
 
 
-async def sender():
-    client = Producer("https://example:8080", use_tls=True)
-    result = await client.rpc.dummy()
-    result = await client.rpc.dummy()
-    result = await client.rpc.dummy()
+def say() -> str:
+    return "Hello!"
 
-    print("first request made, doing the rest...")
-    start_time = time.time()
-    for i in range(100):
-        await client.rpc.dummy()
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Request batch completed. Elapsed time: {elapsed_time:.2f} seconds")
+
+class TestClass:
+    def __init__(
+            self,
+            name: str,
+            email: str
+    ) -> None:
+        self.name = name
+        self.email = email
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_email(self) -> str:
+        return self.email
+
+
+app.add_function(dummy)
+app.add_function(say)
+app.add_class_instance(TestClass("John", "john@email.com"))
+
+
+async def main():
+    client = Producer("https://vadim-seliukov-quic-server.com:8080", use_tls=True, timeout=None)     # vadim-seliukov-quic-server.com
+    functions = await client.discover()
+    print(functions)
+    await client.call("dummy")
     await client.close()
 
 
 if __name__ == "__main__":
-    asyncio.run(sender())
-    # asyncio.run(app.run())
+    # asyncio.run(main())
+    asyncio.run(app.run())
 
 
