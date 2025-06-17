7908a19f3142d00da7356d79ad3a99c77345b1b2
kermit-frogs19 <dadandboom@gmail.com>
2025-06-18 01:24:34 +0300

    feat: main simplified

--- main.py
+++ main.py
@@ -1,90 +1,19 @@
 from ezRPC.receiver.receiver import Receiver
-from ezRPC.producer.producer import Producer
-from ezh3 import Client
 import asyncio
-import time
-import logging
-
-try:
-    import uvloop
-
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-    print("system: Using uvloop for asyncio event loop")
-except ImportError:
-    uvloop = None
-    print("system: Failed to import/connect uvloop for asyncio event loop")
 
 
-app = Receiver(
-    enable_tls=False,
-    custom_cert_file_loc="/app/cert.pem",
-    custom_cert_key_file_loc="/app/key.pem",
-    port=8080,
-    enable_ipv6=True
-)
-
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s %(name)s %(message)s",
-    level=logging.DEBUG,
-)
+app = Receiver(enable_tls=False)
 
 
-@app.function()
+@app.function(description="Get the sum of 2 integers")
 async def get_sum(a: int, b: int) -> int:
-    print(a)
-    print(b)
+    print(f"Got new call for function get_sum: a = {a}, b = {b}")
     result = a + b
+    print(f"Call result is - {result}")
     return result
-
-
-@app.function()
-async def get_subtraction(a: int, b: int) -> int:
-    print(a)
-    print(b)
-    result = a - b
-    return result
-
-
-@app.function()
-async def dummy():
-    return
-
-
-def say() -> str:
-    return "Hello!"
-
-
-class TestClass:
-    def __init__(
-            self,
-            name: str,
-            email: str
-    ) -> None:
-        self.name = name
-        self.email = email
-
-    def get_name(self) -> str:
-        return self.name
-
-    def get_email(self) -> str:
-        return self.email
-
-
-app.add_function(dummy)
-app.add_function(say)
-app.add_class_instance(TestClass("John", "john@email.com"))
-
-
-async def main():
-    client = Producer("https://127.0.0.1:8000", use_tls=True, timeout=None)
-    functions = await client.discover()
-    print(functions)
-    await client.call("dummy")
-    await client.close()
 
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-    asyncio.run(app.run())
+    asyncio.run(app.run(host="0.0.0.0", port=8000))
 
 
